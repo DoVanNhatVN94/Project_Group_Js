5379ed53e37d85f7005ba261434fc01d05e79487
DoVanNhatVN94 <dovannhatdn94@gmail.com>
2022-02-13 13:43:48 +0700

    fix html

---
+++ assets/js/menu.js
@@ -0,0 +1,9 @@
+ /*---stickey menu---*/
+ $(window).on('scroll',function() {    
+    var scroll = $(window).scrollTop();
+    if (scroll < 100) {
+     $(".sticky-header").removeClass("sticky");
+    }else{
+     $(".sticky-header").addClass("sticky");
+    }
+});
