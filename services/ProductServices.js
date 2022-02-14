ae8e512df61a1e6a7cd2e421875c708dd1076ac0
Nam <phamhainamkt@gmail.com>
2022-02-14 10:31:07 +0700

    sửa trang admin

--- services/ProductServices.js
+++ services/ProductServices.js
@@ -4,32 +4,32 @@ export default class ProductServices {
     getProduct() {
         return axios({
             method: 'get',
-            url: 'https://61ebb6137ec58900177cdd3d.mockapi.io/Product',
+            url: 'https://62060a0b161670001741beb6.mockapi.io/sp',
         });
     }
     addProduct(product) {
         return axios({
             method: 'post',
-            url: 'https://61ebb6137ec58900177cdd3d.mockapi.io/Product',
+            url: 'https://62060a0b161670001741beb6.mockapi.io/sp',
             data: product,
         });
     }
     deleteProduct(id) {
         return axios({
             method: 'delete',
-            url: `https://61ebb6137ec58900177cdd3d.mockapi.io/Product/${id}`,
+            url: `https://62060a0b161670001741beb6.mockapi.io/sp/${id}`,
         });
     }
     watchProduct(id) {
         return axios({
             method: 'get',
-            url: `https://61ebb6137ec58900177cdd3d.mockapi.io/Product/${id}`,
+            url: `https://62060a0b161670001741beb6.mockapi.io/sp/${id}`,
         });
     }
     updateProduct(id, product) {
         return axios({
             method: 'put',
-            url: `https://61ebb6137ec58900177cdd3d.mockapi.io/Product/${id}`,
+            url: `https://62060a0b161670001741beb6.mockapi.io/sp/${id}`,
             data: product,
         });
     }
